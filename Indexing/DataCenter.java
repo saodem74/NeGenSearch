7704d198b381c7cf84c8689b513a42e26b37081c
Henry's PC <hguo2003@gmail.com>
2019-09-18 18:19:14 -0500

    Add Part of Data Center

---
+++ Indexing/DataCenter.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class DataCenter {
+    public List<String> GetLineList(String DirPath){
+        List<String> listOfFiles = new ArrayList<String>();
+        listOfFiles = new InputPages().GetPath(DirPath);
+
+        String SaveLine = "";
+        String FileNameString = "";
+        List<String> LineList = new ArrayList<String>();
+        //Read Each file in directory
+        for (int i =0; i < listOfFiles.size(); i++){
+            FileNameString = listOfFiles.get(i);
+            SaveLine = new InputPages().Page2Line(DirPath+FileNameString);
+            LineList.add(SaveLine); //Save Webpage Line in "LineList"
+        }
+        return LineList;
+    }
+}
--- Indexing/IndexMain.java
+++ Indexing/IndexMain.java
@@ -3,20 +3,30 @@ import java.util.*;
 public class IndexMain {
     public static void main(String[] args) {
         List<String> listOfFiles = new ArrayList<String>();
-        listOfFiles = new InputPages().GetPath("./Crawler/src/main/resources/crawledData/pages/");
+        listOfFiles = new InputPages().GetPath("./Crawler/src/main/resources/crawledData/pages/"); //Get Files list for the dictionary
 
-        String SaveLine = "";
-        String FileNameString = "";
         List<String> LineList = new ArrayList<String>();
         //Read Each file in directory
-        for (int i =0; i < listOfFiles.size(); i++){
-            FileNameString = listOfFiles.get(i);
-            SaveLine = new InputPages().Page2Line("./Crawler/src/main/resources/crawledData/pages/"+FileNameString);
-            LineList.add(SaveLine); //Save Webpage Line in "LineList"
-            //System.out.println(SaveLine);
+        LineList = new DataCenter().GetLineList("./Crawler/src/main/resources/crawledData/pages/");
+
+        List<String> IndexList = new ArrayList<String>();
+        for (int i = 0; i < LineList.size(); i++) {
+            String FileNum = listOfFiles.get(i).replace("page","");
+            IndexList.add(FileNum+",1");
+            //System.out.println(IndexList.get(i));
+            String TempLine = LineList.get(i).replaceAll("[\\pP\\p{Punct}]","");
+            //String[] arrOfStr = TempLine.split(" ");
+            for (int count=0; count < TempLine.length(); count++){
+                if (TempLine.charAt(count) == ' '){
+                    IndexList.add(FileNum+","+Integer.toString(count+2));
+                }
+            }
+
+            //for (String a : IndexList)
+            //    System.out.println(a);
+            //System.out.println(LineList.get(i).charAt(0));
         }
-        //for (int i = 0; i < LineList.size(); i++)
-        //   System.out.println(LineList.get(i));
+
 
         String[] ClassOne = { "Kring", "Panda", "Soliel", "Darryl", "Chan", "Matang", "Jollibee.", "Inasal" };
         String[] ClassTwo = { "Minnie", "Kitty", "Madonna", "Miley", "Zoom-zoom", "Cristine", "Bubbles", "Ara", "Rose", "Maria1", "Maria10", "Maria9" };
