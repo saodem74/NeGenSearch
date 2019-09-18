161ab8f644687d7f8f7c35bdad8ce05c666b3f07
Henry's PC <hguo2003@gmail.com>
2019-09-18 00:01:58 -0500

    Add List of Files

--- Indexing/IndexMain.java
+++ Indexing/IndexMain.java
@@ -1,10 +1,22 @@
+import java.io.File;
+import java.util.*;
 public class IndexMain {
     public static void main(String[] args) {
+        List<String> listOfFiles = new ArrayList<String>();
+        listOfFiles = new InputPages().GetPath("./Crawler/src/main/resources/crawledData/pages/");
+
+        String[] FileNameArr = new String[listOfFiles.size()];
+        for (int i =0; i < listOfFiles.size(); i++)
+            FileNameArr[i] = listOfFiles.get(i);
+        for (String x : FileNameArr)
+            System.out.print(x + "\n");
+
         String SaveLine = "";
         SaveLine = new InputPages().Page2Line("./Crawler/src/main/resources/crawledData/pages/page1");
-        System.out.println(SaveLine);
+        //System.out.println(SaveLine);
+
         String[] ClassOne = { "Kring", "Panda", "Soliel", "Darryl", "Chan", "Matang", "Jollibee.", "Inasal" };
-        String[] ClassTwo = { "Minnie", "Kitty", "Madonna", "Miley", "Zoom-zoom", "Cristine", "Bubbles", "Ara", "Rose", "Maria123", "Maria123" };
+        String[] ClassTwo = { "Minnie", "Kitty", "Madonna", "Miley", "Zoom-zoom", "Cristine", "Bubbles", "Ara", "Rose", "Maria1", "Maria10", "Maria9" };
         String[] names = new String[ClassOne.length + ClassTwo.length];
 
         IndexMergeSort.mergeSort(ClassOne);
--- Indexing/InputPages.java
+++ Indexing/InputPages.java
@@ -1,8 +1,11 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.File;
+import java.util.*;
 
 public class InputPages {
+
     public String Page2Line(String filePath) {
         BufferedReader reader;
         String ReadLine = "";
@@ -17,5 +20,17 @@ public class InputPages {
             e.printStackTrace();
         }
         return ReadLine;
+    }
+
+    public List<String> GetPath(String DirPath){
+        File folder = new File(DirPath);
+        File[] listOfFiles = folder.listFiles();
+        List<String> PathFile = new ArrayList<String>();
+        for (int i = 0; i < listOfFiles.length; i++) {
+            if (listOfFiles[i].isFile()) {
+                PathFile.add(listOfFiles[i].getName() + ".txt");
+            }
+        }
+        return PathFile;
     }
 }
