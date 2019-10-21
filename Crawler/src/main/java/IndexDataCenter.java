fa7a09496a76a5da218aeae2dbec13924c1ea5cd
saodem74 <trantrunghieu7492@gmail.com>
2019-10-21 11:48:35 -0500

    Update IndexDataCenter.java

--- Crawler/src/main/java/IndexDataCenter.java
+++ Crawler/src/main/java/IndexDataCenter.java
@@ -89,7 +89,7 @@ public class IndexDataCenter {
 		for (int[] idx : indexes) {
 			String s = getContentFromIndexies(idx[0], idx[1]);
 			sb.append(s).append("\n");
-			sb2.append(s.substring(0, 50)).append("...\n")
+			sb2.append(s.substring(0, 50)).append("...\n");
 		}
 		FileWriter.writeStringToFile(Config.FullSortedData, sb.toString());
 		FileWriter.writeStringToFile(Config.FullSortedData_ShortVersion, sb2.toString());
