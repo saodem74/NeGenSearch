37046c776445e22b6a7c18054b72f7c467aa26ff
saodem74 <trantrunghieu7492@gmail.com>
2019-10-21 12:38:53 -0500

    search

---
+++ Crawler/src/main/java/BasicSearchController.java
@@ -0,0 +1,111 @@
+import utils.FileReader;
+
+import java.util.*;
+
+/**
+ * @author Harry Tran on 10/21/19.
+ * @project Crawler
+ * @email trunghieu.tran@utdallas.edu
+ * @organization UTDallas
+ */
+public class BasicSearchController {
+	static private Map<Integer, String> mapIDtoURL = new HashMap<>();
+	static private Map<Integer, List<String>> mapIDtoContent = new HashMap<>();
+	static private int numOfUrl = 0;
+	static private List<int[]> indexes = null;
+
+	private static void init() {
+		indexes = new ArrayList<>();
+
+		String data = FileReader.readStringFromFile(Config.IndexedFile);
+		String[] lines = data.split("\n");
+		for (String line : lines) {
+			String[] ss = line.split(" ");
+			int[] tmp = {Integer.parseInt(ss[0], Integer.parseInt(ss[1]))};
+			indexes.add(tmp);
+		}
+
+		data = FileReader.readStringFromFile(Config.IdToURL);
+		lines = data.split("\n");
+		for (String line : lines) {
+			String[] ss = line.split(" ");
+			mapIDtoURL.put(Integer.parseInt(ss[0]), ss[1]);
+		}
+
+		data = FileReader.readStringFromFile(Config.IdToContent);
+		lines = data.split("\n");
+		for (String line : lines) {
+			String[] ss = line.split(" ");
+			List<String> content = new ArrayList<>();
+			for (int i = 1; i < ss.length; ++i)
+				content.add(ss[i]);
+			mapIDtoContent.put(Integer.parseInt(ss[0]), content);
+		}
+		numOfUrl = mapIDtoContent.keySet().size();
+	}
+
+	public static String getContentFromIndexies(int id, int shift) {
+		List<String> str = mapIDtoContent.get(id);
+		StringBuilder sb = new StringBuilder();
+		for (int i = shift; i < str.size(); ++i)
+			sb.append(str.get(i)).append(" ");
+		for (int i = 0; i < shift; ++i)
+			sb.append(str.get(i)).append(" ");
+		return sb.toString();
+	}
+
+	public static List<String[]> getResultQuery(String query) {
+		if (indexes == null) init();
+
+		List<String[]> res = new ArrayList<>();
+		Set<Integer> inq = new HashSet<>();
+
+		int l = 0, r = indexes.size() - 1;
+		int pos = -1;
+		while (l <= r) {
+			int mid = (l + r) / 2;
+			String tmpStr = getContentFromIndexies(indexes.get(mid)[0], indexes.get(mid)[1]);
+			if (tmpStr.startsWith(query)) {
+				pos = mid;
+				break;
+			}
+			if (tmpStr.compareToIgnoreCase(query) < 0) l = mid + 1;
+			else r = mid - 1;
+		}
+
+		if (pos != -1) {
+			String[] tmp = {mapIDtoURL.get(indexes.get(pos)[0]), getContentFromIndexies(indexes.get(pos)[0], indexes.get(pos)[1])};
+			inq.add(indexes.get(pos)[0]);
+			res.add(tmp);
+			int i = pos - 1;
+			while (i >= 0) {
+				String tmpStr = getContentFromIndexies(indexes.get(i)[0], indexes.get(i)[1]);
+				if (tmpStr.startsWith(query)) {
+					String[] tmpi = {mapIDtoURL.get(indexes.get(i)[0]), getContentFromIndexies(indexes.get(i)[0], indexes.get(i)[1])};
+					inq.add(indexes.get(i)[0]);
+					res.add(tmpi);
+					--i;
+				} else break;
+			}
+
+			i = pos + 1;
+			while (i < indexes.size()) {
+				String tmpStr = getContentFromIndexies(indexes.get(i)[0], indexes.get(i)[1]);
+				if (tmpStr.startsWith(query)) {
+					String[] tmpi = {mapIDtoURL.get(indexes.get(i)[0]), getContentFromIndexies(indexes.get(i)[0], indexes.get(i)[1])};
+					inq.add(indexes.get(i)[0]);
+					res.add(tmpi);
+					++i;
+				} else break;
+			}
+		}
+		return res;
+	}
+
+	public static void main(String[] args) {
+		List<String[]> res = BasicSearchController.getResultQuery("Computer");
+		for (int i = 0; i < res.size(); ++i) {
+			System.out.println(res.get(i)[0]);
+		}
+	}
+}
