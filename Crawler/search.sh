2b7ae224e9c454e540b7e402fef6228578c0fc86
saodem74 <trantrunghieu7492@gmail.com>
2019-10-29 15:04:04 -0500

    edit search.sh

--- Crawler/search.sh
+++ Crawler/search.sh
@@ -1,1 +1,5 @@
+#!/bin/sh
+# change this line
+cd /Users/tranhieu/Dropbox/Fall2019/SW_architecuture/project/Crawler/ # change it into directory where you push Cralwer
+mvn compile
 mvn exec:java -Dexec.mainClass="BasicSearchController" -Dexec.args=$1
